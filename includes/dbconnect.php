7e2d3a84ab65b7536a5584b843b84ae8a87f8a55
Haritha Nandhini <harithanandhini2001@gmail.com>
2021-10-13 21:56:04 +0530

    footer

---
+++ includes/dbconnect.php
@@ -0,0 +1,9 @@
+<?php
+$connection=mysqli_connect("127.0.0.1","root","","vivproject");
+if(!$connection){
+ //   echo "success";
+//}
+//else{
+    die("Error".mysqli_connect_error());
+}
+?>
---
+++ includes/landing_nav.php
@@ -0,0 +1,9 @@
+<nav class="navbar">
+                <a href="#" class="navbar-brand"><img src="assets/logo.jpg" alt="logo image" style="width:70px;"></a>
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a href="mainpage.php" class="nav-link custom-font-color" >Home</a></li><div class = "vertical"></div>
+                    <li class="nav-item"><a href="#shop" class="nav-link custom-font-color">Shop</a></li><div class = "vertical1"></div>
+                    <li class="nav-item"><a href="#aboutus" class="nav-link custom-font-color">About Us</a></li><div class = "vertical2"></div>
+                    <li class="nav-item"><a href="#contactus" class="nav-link custom-font-color">Contact Us</a></li>
+                </ul>
+            </nav>
