4cf2c6f6bea5bc7029d642d42f10a08400205f55
Haritha Nandhini <harithanandhini2001@gmail.com>
2021-10-13 22:05:23 +0530

    commit

--- registerfetch.php
+++ registerfetch.php
@@ -1,23 +1,16 @@
 <?php
-$connection=mysqli_connect("localhost","root","","phptestfile");
-//print_r($connection);
-if($connection===false){
-    die("Something went wrong with your connection".mysqli_connect_error());
-}
-echo "connection created successfully<br>";
-echo "Connection host is as follows<br>".mysqli_get_host_info($connection);
-$name=$_POST['name'];
-$email=$_POST['email'];
-$password=$_POST['password'];
-$created_by=1;
-$query="INSERT INTO users(name,email,password,created_by)VALUE('$name','$email','$password','$created_by')";
-if(mysqli_query($connection,$query))
-{
-echo "<br>Data has been inserted successfully";
-}
-else{
-    echo "<br>Data could not be inserted";
-    echo "<br>Check error trace<br>".mysqli_error($connection);
-}
-mysqli_close($connection);
+    $usertype=$_POST['usertype'];
+    $name=$_POST['name'];
+    $phoneno=$_POST['phoneno'];
+    $emailid=$_POST['emailid'];
+    $password=$_POST['password'];
+    $connection= new mysqli_connect("localhost","root","","finalproject");
+        if(mysqli_connect_error()){
+            die('Connect Error('.mysqli_connect_error().')'. mysqli_connect_error());
+        }else{
+        $sql="INSERT INTO regis(usertype,name,phoneno,emailid,password) VALUE ('$usertype','$name', '$phoneno', '$emailid', '$password')";
+            echo "Data has been inserted";
+        }
+        mysqli_close($connection);
+        header("location:login.php");
 ?>
